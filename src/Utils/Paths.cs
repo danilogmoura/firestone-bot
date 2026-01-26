09374d77fcb381ac467905fce1845841d1dfaf31
Danilo Moura <danilogmoura@gmail.com>
2026-01-26 00:50:48 -0300

    Refactor Paths to improve code readability; streamline class structure and enhance consistency in path definitions

--- src/Utils/Paths.cs
+++ src/Utils/Paths.cs
@@ -1,134 +1,136 @@
-﻿namespace Firebot.Utils
+﻿namespace Firebot.Utils;
+
+public abstract class Paths
 {
-    public abstract class Paths
+    private const string BattleUI = "battleRoot/battleMain/battleCanvas/SafeArea/leftSideUI";
+
+    private const string NotificationsGrid = BattleUI + "/notifications/Viewport/grid";
+
+    private const string MenuBase = "menusRoot/menuCanvasParent/SafeArea/menuCanvas";
+
+    private const string Menus = MenuBase + "/menus";
+
+    private const string Popups = MenuBase + "/popups";
+
+    public const string EventsPopupPath = MenuBase + "/events";
+
+    internal static class Engineer
     {
-        private const string BattleUI = "battleRoot/battleMain/battleCanvas/SafeArea/leftSideUI";
+        public const string EngineerGridNotification = NotificationsGrid + "/Engineer";
 
-        private const string NotificationsGrid = BattleUI + "/notifications/Viewport/grid";
+        public const string ClaimToolsButton =
+            Menus + "/Engineer/submenus/bg/engineerSubmenu/toolsProductionSection/claimToolsButton";
 
-        private const string MenuBase = "menusRoot/menuCanvasParent/SafeArea/menuCanvas";
+        public const string CloseButton = Menus + "/Engineer/closeButton";
+    }
 
-        private const string Menus = MenuBase + "/menus";
+    internal static class WarfrontCampaign
+    {
+        public const string WarfrontCampaignNotification = NotificationsGrid + "/WarfrontCampaign";
 
-        private const string Popups = MenuBase + "/popups";
+        public const string CloseButton = Menus + "/WorldMap/closeButton";
 
-        public const string EventsPopupPath = MenuBase + "/events";
+        public const string ClaimToolsPath = Menus + "/WorldMap/submenus/warfrontCampaignSubmenu/loot";
 
-        internal static class Engineer
-        {
-            public const string EngineerGridNotification = NotificationsGrid + "/Engineer";
+        public const string ClaimToolsButton = ClaimToolsPath + "/claimButton";
 
-            public const string ClaimToolsButton =
-                Menus + "/Engineer/submenus/bg/engineerSubmenu/toolsProductionSection/claimToolsButton";
+        public const string NextLootTimeLeft = ClaimToolsPath + "/nextLootTimeLeft";
+    }
 
-            public const string CloseButton = Menus + "/Engineer/closeButton";
-        }
+    internal static class Missions
+    {
+        public const string MenuWorldMap = Menus + "/WorldMap";
 
-        internal static class WarfrontCampaign
-        {
-            public const string WarfrontCampaignNotification = NotificationsGrid + "/WarfrontCampaign";
+        public const string MissionRegion = "menusRoot/mapRoot/mapElements/missions";
 
-            public const string ClaimToolsButton =
-                Menus + "/WorldMap/submenus/warfrontCampaignSubmenu/loot/claimButton";
+        public const string SquadsQuantity = MenuWorldMap + "/counters/squads/quantity";
 
-            public const string CloseButton = Menus + "/WorldMap/closeButton";
-        }
+        public const string StartMissionButton =
+            Popups + "/PreviewMission/bg/managementBg/container/startMissionButton";
 
-        internal static class Missions
-        {
-            public const string MenuWorldMap = Menus + "/WorldMap";
+        public const string MissionRefreshTime =
+            MenuWorldMap +
+            "/submenus/mapMissionsSubmenu/bottomLeftUI/missionRefreshCanvas/missionRefreshBg/missionRefreshText";
 
-            public const string MissionRegion = "menusRoot/mapRoot/mapElements/missions";
+        public const string MissionCloseButton = MenuWorldMap + "/closeButton";
 
-            public const string SquadsQuantity = MenuWorldMap + "/counters/squads/quantity";
+        public const string MapMissionNotification = NotificationsGrid + "/MapMissions";
+    }
 
-            public const string StartMissionButton =
-                Popups + "/PreviewMission/bg/managementBg/container/startMissionButton";
+    internal static class Expedition
+    {
+        public const string ExpeditionNotification = NotificationsGrid + "/Expeditions";
 
-            public const string MissionRefreshTime =
-                MenuWorldMap +
-                "/submenus/mapMissionsSubmenu/bottomLeftUI/missionRefreshCanvas/missionRefreshBg/missionRefreshText";
+        public const string ExpeditionsParents = Popups + "/Expeditions/bg/expeditionsParent";
 
-            public const string MissionCloseButton = MenuWorldMap + "/closeButton";
+        public const string CurrentExpeditionPath = ExpeditionsParents + "/activeExpeditionParent/activeExpedition";
 
-            public const string MapMissionNotification = NotificationsGrid + "/MapMissions";
-        }
+        public const string PendingExpeditionPath =
+            ExpeditionsParents + "/pendingExpeditionsParent/expeditionsScroll/Viewport/grid/expeditionPending0";
 
-        internal static class Expedition
-        {
-            public const string ExpeditionNotification = NotificationsGrid + "/Expeditions";
+        public const string CloseButton = Popups + "/Expeditions/bg/closeButton";
+    }
 
-            public const string ExpeditionsParents = Popups + "/Expeditions/bg/expeditionsParent";
+    internal static class FirestoneResearch
+    {
+        public const string LibrarySubmenus = Menus + "/Library/submenus";
 
-            public const string CurrentExpeditionPath = ExpeditionsParents + "/activeExpeditionParent/activeExpedition";
+        public const string FirestoneResearchNotificationPath = NotificationsGrid + "/FirestoneResearch";
 
-            public const string PendingExpeditionPath =
-                ExpeditionsParents + "/pendingExpeditionsParent/expeditionsScroll/Viewport/grid/expeditionPending0";
+        public const string MissionCloseButton = Menus + "/Library/closeButton";
 
-            public const string CloseButton = Popups + "/Expeditions/bg/closeButton";
-        }
+        public const string ResearchPanelDownPath = LibrarySubmenus + "/firestoneResearch/researchPanel";
 
-        internal static class FirestoneResearch
-        {
-            public const string LibrarySubmenus = Menus + "/Library/submenus";
+        public const string SelectResearchTablePath =
+            LibrarySubmenus + "/firestoneResearch/researchPanel/selectResearchTable";
 
-            public const string FirestoneResearchNotificationPath = NotificationsGrid + "/FirestoneResearch";
+        public const string SubmenusTreePath =
+            LibrarySubmenus + "/firestoneResearch/researchScrollView/viewport/content/submenus";
 
-            public const string MissionCloseButton = Menus + "/Library/closeButton";
+        public const string PopupActivateButton =
+            Popups + "/FirestoneResearchPreview/bg/innerBg/unlocked/buttonHolder/researchActivateButton";
+    }
 
-            public const string ResearchPanelDownPath = LibrarySubmenus + "/firestoneResearch/researchPanel";
+    internal static class OracleRituals
+    {
+        public const string OracleRitualNotification = NotificationsGrid + "/OracleRituals";
 
-            public const string SelectResearchTablePath =
-                LibrarySubmenus + "/firestoneResearch/researchPanel/selectResearchTable";
+        public const string MenuOracle = Menus + "/Oracle";
 
-            public const string SubmenusTreePath =
-                LibrarySubmenus + "/firestoneResearch/researchScrollView/viewport/content/submenus";
+        public const string CloseButton = MenuOracle + "/closeButton";
 
-            public const string PopupActivateButton =
-                Popups + "/FirestoneResearchPreview/bg/innerBg/unlocked/buttonHolder/researchActivateButton";
-        }
+        public const string RitualGrid = MenuOracle + "/submenus/bg/ritualSubmenu/ritualsGrid";
+    }
 
-        internal static class OracleRituals
-        {
-            public const string OracleRitualNotification = NotificationsGrid + "/OracleRituals";
+    internal static class BattleRoot
+    {
+        public const string OfflineProgressPopup = MenuBase + "/popups/OfflineProgress";
 
-            public const string MenuOracle = Menus + "/Oracle";
+        public const string OfflineProgressPopupClaimButton = OfflineProgressPopup + "/bg/collectButton";
+    }
 
-            public const string CloseButton = MenuOracle + "/closeButton";
+    internal static class GuardianTraining
+    {
+        public const string GuardianTrainingNotification = NotificationsGrid + "/GuardianTraining";
 
-            public const string RitualGrid = MenuOracle + "/submenus/bg/ritualSubmenu/ritualsGrid";
-        }
+        public const string MenuMagicQuarters = Menus + "/MagicQuarters";
 
-        internal static class BattleRoot
-        {
-            public const string OfflineProgressPopup = MenuBase + "/popups/OfflineProgress";
+        public const string GuardianList = MenuMagicQuarters + "/guardianList";
 
-            public const string OfflineProgressPopupClaimButton = OfflineProgressPopup + "/bg/collectButton";
-        }
+        public const string CloseButton = MenuMagicQuarters + "/closeButton";
 
-        internal static class GuardianTraining
-        {
-            public const string GuardianTrainingNotification = NotificationsGrid + "/GuardianTraining";
+        public const string TrainingButton =
+            MenuMagicQuarters + "/submenus/bg/infoSubmenu/activities/unlocked/train/trainButton";
+    }
 
-            public const string MenuMagicQuarters = Menus + "/MagicQuarters";
+    internal static class GuildShop
+    {
+        public const string FreePickaxesNotificationPath = NotificationsGrid + "/FreePickaxes";
 
-            public const string GuardianList = MenuMagicQuarters + "/guardianList";
+        public const string MenuGuildShopPath = Menus + "/GuildShop";
 
-            public const string CloseButton = MenuMagicQuarters + "/closeButton";
+        public const string CloseButtonPath = MenuGuildShopPath + "/closeButton";
 
-            public const string TrainingButton =
-                MenuMagicQuarters + "/submenus/bg/infoSubmenu/activities/unlocked/train/trainButton";
-        }
-
-        internal static class GuildShop
-        {
-            public const string FreePickaxesNotificationPath = NotificationsGrid + "/FreePickaxes";
-
-            public const string MenuGuildShopPath = Menus + "/GuildShop";
-
-            public const string CloseButtonPath = MenuGuildShopPath + "/closeButton";
-
-            public const string FreePickaxeItemPath = MenuGuildShopPath + "/bg/submenus/supplies/items/freePickaxe";
-        }
+        public const string FreePickaxeItemPath = MenuGuildShopPath + "/bg/submenus/supplies/items/freePickaxe";
     }
 }
