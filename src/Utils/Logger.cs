3c81fa5b3ca7e41184800072f79b32cce9ac0b9c
Danilo Moura <danilogmoura@gmail.com>
2026-01-26 22:32:03 -0300

    Add Logger class for improved logging functionality

---
+++ src/Utils/Logger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Firebot.Core;
+using MelonLoader;
+
+namespace Firebot.Utils;
+
+public class Logger
+{
+    private const ConsoleColor TextColor = ConsoleColor.Gray;
+    private const ConsoleColor DebugColor = ConsoleColor.Blue;
+
+    private readonly string _logTag;
+
+    public Logger(string logTag)
+    {
+        _logTag = logTag;
+    }
+
+
+    public void Info(string message) => MelonLogger.Msg(TextColor, $"[{_logTag}] {message}");
+
+    public void Warning(string message) => MelonLogger.Warning($"[{_logTag}] {message}");
+
+    public void Error(string message) => MelonLogger.Error($"[{_logTag}] {message}");
+
+    public void Debug(string message)
+    {
+        if (BotSettings.DebugMode || MelonDebug.IsEnabled())
+            MelonLogger.Msg(DebugColor, $"[DEBUG] [{_logTag}] {message}");
+    }
+}
