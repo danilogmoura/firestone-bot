bac12dbf3c2ba75c4ad4bd930968562c5b8d06a4
Danilo Moura <danilogmoura@gmail.com>
2026-03-03 03:38:23 -0300

    feat: Add Upgrades class for handling upgrade button interactions

---
+++ src/GameModel/Features/MainScene/Upgrades.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using Firebot.GameModel.Primitives;
+using Firebot.Infrastructure;
+
+namespace Firebot.GameModel.Features.MainScene;
+
+public static class Upgrades
+{
+    public static IEnumerator Close =>
+        new GameButton(Paths.MenusLoc.CanvasLoc.MainSceneLoc.UpgradesLoc.CloseBtn).Click();
+}
