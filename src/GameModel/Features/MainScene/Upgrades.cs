c6785abe1ac37ace72c24fb7ac7e9b17f3b7609b
Danilo Moura <danilogmoura@gmail.com>
2026-03-03 20:07:42 -0300

    feat: Add visibility check and upgrade level handling to Upgrades class

--- src/GameModel/Features/MainScene/Upgrades.cs
+++ src/GameModel/Features/MainScene/Upgrades.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using Firebot.GameModel.Base;
 using Firebot.GameModel.Primitives;
 using Firebot.Infrastructure;
 
@@ -6,6 +7,33 @@ namespace Firebot.GameModel.Features.MainScene;
 
 public static class Upgrades
 {
+    public static bool IsVisible =>
+        new GameElement(Paths.MenusLoc.CanvasLoc.MainSceneLoc.UpgradesLoc.Root).IsVisible();
+
     public static IEnumerator Close =>
         new GameButton(Paths.MenusLoc.CanvasLoc.MainSceneLoc.UpgradesLoc.CloseBtn).Click();
+
+    private static string LvlUpNumTxt =>
+        new GameText(Paths.MenusLoc.CanvasLoc.MainSceneLoc.UpgradesLoc.GuardianUpgradeLvlTxt).GetParsedText();
+
+    private static IEnumerator ChangeLevelUpModeBtn =>
+        new GameButton(Paths.MenusLoc.CanvasLoc.MainSceneLoc.UpgradesLoc.ChangeLevelUpModeButton).Click();
+
+    /// <summary>
+    ///     Sets the upgrade level. The default value is x25.
+    /// </summary>
+    public static IEnumerator SetUpgradeLevel()
+    {
+        if (!IsVisible) yield break;
+
+        var currentLevel = LvlUpNumTxt;
+        if (currentLevel == "x25") yield break;
+
+
+        while (currentLevel != "x25")
+        {
+            yield return ChangeLevelUpModeBtn;
+            currentLevel = LvlUpNumTxt;
+        }
+    }
 }
