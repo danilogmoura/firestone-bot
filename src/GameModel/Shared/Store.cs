4c5e24a367c72b0b40228c6a70d892212289e107
Danilo Moura <danilogmoura@gmail.com>
2026-03-05 20:35:08 -0300

    feat: Add DailyRewardsTask, Store, and OracleStore classes for managing daily rewards

---
+++ src/GameModel/Shared/Store.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using Firebot.GameModel.Primitives;
+using Firebot.Infrastructure;
+
+namespace Firebot.GameModel.Shared;
+
+public static class Store
+{
+    public static IEnumerator Close =>
+        new GameButton(Paths.MenusLoc.CanvasLoc.StoreLoc.CloseBtn).Click();
+
+    public static IEnumerator ClaimCheckIn =>
+        new GameButton(Paths.MenusLoc.CanvasLoc.StoreLoc.CheckInLoc.CheckInBtn).Click();
+
+    public static DateTime CheckInNextRunTime =>
+        new GameText(Paths.MenusLoc.CanvasLoc.StoreLoc.CheckInLoc.NextRunTimeTxt).Time;
+
+    public static IEnumerator ClaimMysteryBox =>
+        new GameButton(Paths.MenusLoc.CanvasLoc.StoreLoc.MysteryBoxLoc.MysteryBoxBtn).Click();
+
+    public static DateTime MysteryBoxNextRunTime =>
+        new GameText(Paths.MenusLoc.CanvasLoc.StoreLoc.MysteryBoxLoc.NextRunTimeTxt).Time;
+}
